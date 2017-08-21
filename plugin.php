2c376380439c78c7dd825e9e9459c3d0d622dbb5
nahroege <egelila@gmail.com>
2017-08-22 01:16:50 +0300

    ilk commit

---
+++ plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Sepet
+Plugin URI: #
+Description: Bir e-ticaret sepet eklentisidir.Bu eklenti sayesinde wordpress
+aracılığla ticaret sitesine dönüşübilecek
+Author: Ege ORHAN
+Version: 0.1
+Author URI: Ege ORHAN
+*/
+?>
